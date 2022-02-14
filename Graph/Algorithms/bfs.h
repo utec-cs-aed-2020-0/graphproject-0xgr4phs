26a0bc3380341a5b383ad755463bedf60b19b23a
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2022-02-14 11:30:29 -0500

    <update>(BFS & DFS) Implementacion de algoritmos

--- Graph/Algorithms/bfs.h
+++ Graph/Algorithms/bfs.h
@@ -8,15 +8,24 @@
 using namespace std;
 
 template <typename TV, typename TE>
-string BFS(Graph<TV, TE> graph, int startIndex)
+string BFS(Graph<TV, TE> graph, int startId)
 {
-    auto* root = graph->getVertex(startId);
+    Vertex<TV,TE>* root = graph->getVertex(startId);
     deque<Vertex<TV,TE>*> pass;
-    bool[] 
+    unordered_map<Vertex<TV,TE>*, bool> visited;
     pass.push_back(root);
+    visited[root] = true;
+    string output = "";
     while (!pass.empty())
     {
-        
+        Vertex<TV,TE>* s = pass.front();
+        output += s->data;
+        pass.pop_front();
+
+        for (Edge<TV,TE>* &edge: s->edges)
+        {
+            edge->vertexes[1]
+        }
     }
     
     delete root;
--- Graph/Algorithms/dfs.h
+++ Graph/Algorithms/dfs.h
@@ -3,13 +3,28 @@
 #include "../DirectedGraph.h"
 #include "../UndirectedGraph.h"
 #include <iostream>
+#include <stack>
 using namespace std;
 template <typename TV, typename TE>
 string DFS(Graph<TV,TE> graph, string startId)
 {
-    auto* root = graph->getVertex(startId);
+    Vertex<TV,TE>* root = graph->getVertex(startId);
+    stack<Vertex<TV,TE>*> pass;
+    unordered_map<Vertex<TV,TE>*, bool> visited;
+    pass.push_back(root);
+    visited[root] = true;
+    string output = "";
+    while (!pass.empty())
+    {
+        Vertex<TV,TE>* s = pass.front();
+        output += s->data;
+        pass.pop();
+        for (Edge<TV,TE>* &edge: s->edges)
+        {
+            edge->vertexes[1]
+            // pass.push()
+        }
+    }
     
     delete root;
-}
-
 #endif // DFS_H
