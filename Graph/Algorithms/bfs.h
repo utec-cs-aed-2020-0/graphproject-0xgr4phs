d43af6a8f65d7ca71b3cb33d6a938ddcd8720cf4
Alexander Morales Panitz <76183040+alexanderutec@users.noreply.github.com>
2022-03-01 01:41:00 -0500

    Update bfs.h

--- Graph/Algorithms/bfs.h
+++ Graph/Algorithms/bfs.h
@@ -25,7 +25,12 @@
 /**
  * Greedy BFS
  * 
- * // TODO: Añadir intro y referencias de la búsqueda
+ * El codicioso algoritmo de búsqueda mejor primero siempre selecciona la ruta que parece mejor
+ * en ese momento. Es la combinación de algoritmos de búsqueda primero en profundidad y primero
+ * en amplitud. Utiliza la función heurística y la búsqueda. La búsqueda Best-first nos permite
+ * aprovechar las ventajas de ambos algoritmos.
+ * 
+ * Fuente: https://bit.ly/3C1sy3E
  */
 
 template <typename TV, typename TE>
@@ -55,17 +60,50 @@ DirectedGraph<TV,TE> greedyBFS(DirectedGraph<TV,TE> graph, std::string start_nod
         std::string id = pq.top().first;
         TV data = pq.top().second;
         travel += std::to_string(data) + " ";
-        // Recorrido
+        Vertex<TV,TE> *current_vertex = graph->vertexes[id];
+        for (Edge<TV,TE> &edge : current_vertex->edges) {
+            Vertex<TV,TE> *connectedVertex = edge->vertexes[1];
+            if (!visited[connectedVertex->id]) {
+                visited[connectedVertex->id] = true;
+                pq.push(make_pair(connectedVertex->id, connectedVertex->data));
+            }
+        }
     }
-        
+
 };
 
 /* Para un grafo no dirigido */
 
 template <typename TV, typename TE>
 UnDirectedGraph<TV,TE> greedyBFS(UnDirectedGraph<TV,TE> graph, std::string start_node, std::string end_node) {
+   std::string travel = "";
+    if (graph->findById(start_node) && graph->findById(end_node)) logic_error("Los nodos parámetros no funcionan correctamente.");
 
+    // Para simplificar el código
+    typedef pair<std::string, Vertex<TV,TE>*> type_pair;
+    std::unordered_map<std::string, bool> visited(graph->V);
+
+    std::priority_queue<type_pair, std::vector<type_pair>, greater<type_pair>> pq;
+
+    Vertex<TV,TE> *start_vertex = graph->vertexes[start_node];
+    Vertex<TV,TE> *end_vertex= graph->vertexes[end_node];
+    visited[start_vertex->id] = true;
+    pq.push(make_pair(start_vertex->id,start_vertex->data));
+    while (pq.empty()) {
+        std::string id = pq.top().first;
+        TV data = pq.top().second;
+        travel += std::to_string(data) + " ";
+        Vertex<TV,TE> *current_vertex = graph->vertexes[id];
+        for (Edge<TV,TE> &edge : current_vertex->edges) {
+            Vertex<TV,TE> *connectedVertex = edge->vertexes[1];
+            if (!visited[connectedVertex->id]) {
+                visited[connectedVertex->id] = true;
+                pq.push(make_pair(connectedVertex->id, connectedVertex->data));
+            }
+        }
+    } 
 };
 
+// TODO: Testear y corregir en tiempo de compilación
 
 #endif // GREEDY_BFS_H
