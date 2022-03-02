202bb503f1d668e5798cb12ebe47b5886386272f
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2022-03-01 21:57:32 -0500

    feat (Prim.h) Algoritmo exclusivo para no dirigidos

--- Graph/Algorithms/prim.h
+++ Graph/Algorithms/prim.h
@@ -12,49 +12,6 @@
 using namespace std;
 
 /**
- * Funcion que emplea el algoritmo de Prim para retornar un grafo (DIRIGIDO) con el arbol de minima expansion
- * Se utiliza una pila para regresar a vertices previamente visitados
- * Si el disjoint set se unifica, se retorna previamente
- * @tparam TV Tipo de dato para la informacion del vertice
- * @tparam TE Tipo de dato para el peso de la arista
- * @param grafo grafo a ser evaluado
- * @param inicio vertice inicial para el algoritmo
- * @return Grafo que representa el arbol de minima expansion
- */
-template<typename TV, typename TE>
-DirectedGraph<TV, TE> prim(DirectedGraph<TV, TE> grafo, string inicio){
-    DirectedGraph<TV, TE> retorno;
-    DisjointSetGrafoDirigido gSet(grafo);
-    stack<Vertex<TV, TE>*> pilaVertice;
-    pilaVertice.push(grafo.vertexes[inicio]);
-    for(const pair<string,Vertex<TV, TE>*>& vertice: grafo.vertexes){
-        retorno.insertVertex(vertice.first, vertice.second->data);
-    }
-
-    while(!pilaVertice.empty()){
-        sort(pilaVertice.top()->edges.begin(), pilaVertice.top()->edges.end(), [](Edge<TV, TE>* a, Edge<TV, TE>* b){return a->weight<b->weight;});
-        std::list<Edge<TV, TE>*>& edgesAlias = pilaVertice.top()->edges;
-        int edgesLength = edgesAlias.size();
-        for(int i = 0; i<edgesLength; i++){
-            if(gSet.Union(edgesAlias.front()->vertexes[0]->id, edgesAlias.front()->vertexes[1]->id)){
-                retorno.createEdge(edgesAlias.front()->vertexes[0]->id, edgesAlias.front()->vertexes[1]->id);
-                pilaVertice.push(edgesAlias.front()->vertexes[1]);
-                break;
-            }
-            edgesAlias.pop_front();
-        }
-        if(edgesAlias.size() == 0) {
-            pilaVertice.pop();
-        }
-        if(gSet.getSets()==1){
-            return retorno;
-        }
-    }
-
-    return retorno;
-}
-
-/**
  * Funcion que emplea el algoritmo de Prim para retornar un grafo (NO DIRIGIDO) con el arbol de minima expansion
  * Se utiliza una pila para regresar a vertices previamente visitados
  * Si el disjoint set se unifica, se retorna previamente
@@ -66,31 +23,32 @@ DirectedGraph<TV, TE> prim(DirectedGraph<TV, TE> grafo, string inicio){
  */
 template<typename TV, typename TE>
 UnDirectedGraph<TV, TE> prim(UnDirectedGraph<TV, TE> grafo, string inicio){
-    UnDirectedGraph<TV, TE> retorno;
+    if(grafo.vertexes.find(inicio) == grafo.vertexes.end()) throw("Vertice no encontrado");
     DisjointSetGrafoNoDirigido gSet(grafo);
+    UnDirectedGraph<TV, TE> retorno;
     stack<Vertex<TV, TE>*> pilaVertice;
     pilaVertice.push(grafo.vertexes[inicio]);
     for(const pair<string,Vertex<TV, TE>*>& vertice: grafo.vertexes){
         retorno.insertVertex(vertice.first, vertice.second->data);
+        vector<Edge<TV, TE>*> aristas(vertice.second->edges.begin(), vertice.second->edges.end());
+        sort(aristas.begin(), aristas.end(),[](Edge<TV, TE>* a, Edge<TV, TE>* b){return a->weight<b->weight;});
+        vertice.second->edges = list<Edge<TV, TE>*>(aristas.begin(), aristas.end());
     }
-
     while(!pilaVertice.empty()){
-        sort(pilaVertice.top()->edges.begin(), pilaVertice.top()->edges.end(), [](Edge<TV, TE>* a, Edge<TV, TE>* b){return a->weight<b->weight;});
-        std::list<Edge<TV, TE>*>& edgesAlias = pilaVertice.top()->edges;
-        int edgesLength = edgesAlias.size();
-        for(int i = 0; i<edgesLength; i++){
-            if(gSet.Union(edgesAlias.front()->vertexes[0]->id, edgesAlias.front()->vertexes[1]->id)){
-                retorno.createEdge(edgesAlias.front()->vertexes[0]->id, edgesAlias.front()->vertexes[1]->id);
-                pilaVertice.push(edgesAlias.front()->vertexes[1]);
+        if(gSet.getSets()==1) return retorno;
+        Vertex<TV, TE>* &temp = pilaVertice.top();
+        int edgesLength = pilaVertice.top()->edges.size();
+        auto i = temp->edges.begin();
+        for(; i!= temp->edges.end(); i++){
+            if(gSet.Union((*i)->vertexes[0]->id, (*i)->vertexes[1]->id)){
+                retorno.createEdge((*i)->vertexes[0]->id, (*i)->vertexes[1]->id, (*i)->weight);
+                pilaVertice.push((*i)->vertexes[1]);
+                grafo.deleteEdge((*i)->vertexes[0]->id, (*i)->vertexes[1]->id);
                 break;
             }
-            edgesAlias.pop_front();
         }
-        if(edgesAlias.size() == 0) {
+        if(i==temp->edges.end()){
             pilaVertice.pop();
-        }
-        if(gSet.getSets()==1){
-            return retorno;
         }
     }
 
