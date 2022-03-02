1a5939777e78f268e2a53b4375c0499f1b2199e3
GU-U7 <grover.ugarte@utec.edu.pe>
2022-03-01 23:55:33 -0500

    fix (Formateo)

--- Graph/Algorithms/data_structures/ds.h
+++ Graph/Algorithms/data_structures/ds.h
@@ -5,7 +5,7 @@
 #include "../../DirectedGraph.h"
 #include "../../Graph.h"
 #include <unordered_map>
-#include<utility>
+#include <utility>
 
 /**
  * Funcion para comparar aristas con respecto al peso
@@ -16,62 +16,76 @@ using namespace std;
 /**
  * Clase que representa el disjoint set para los vertices de un grafo
  */
-class DisjointSetGrafoDirigido{
+class DisjointSetGrafoDirigido
+{
 private:
     unordered_map<string, string> almacen;
     int sets;
+
 public:
-    template<typename TV, typename TE>
-    DisjointSetGrafoDirigido(DirectedGraph<TV, TE> &grafo){
+    template <typename TV, typename TE>
+    DisjointSetGrafoDirigido(DirectedGraph<TV, TE> &grafo)
+    {
         sets = 0;
-        for(auto vertice: grafo.vertexes){
+        for (auto vertice : grafo.vertexes)
+        {
             almacen[vertice.first] = {vertice.first, 0};
             sets++;
         }
     }
-    string Find(string id){
-        return (almacen[id]!=id?Find(almacen[id]):id);
+    string Find(string id)
+    {
+        return (almacen[id] != id ? Find(almacen[id]) : id);
     }
-    bool Union(string id1, string id2){
+    bool Union(string id1, string id2)
+    {
         string raiz1 = Find(id1), raiz2 = Find(id2);
-        if(raiz1!=raiz2){
+        if (raiz1 != raiz2)
+        {
             almacen[raiz2] = raiz1;
             sets--;
             return true;
         }
         return false;
     }
-    int getSets(){return sets;}
-    ~DisjointSetGrafoDirigido(){}
+    int getSets() { return sets; }
+    ~DisjointSetGrafoDirigido() {}
 };
 
-class DisjointSetGrafoNoDirigido{
+class DisjointSetGrafoNoDirigido
+{
 private:
     unordered_map<string, string> almacen;
     int sets;
+
 public:
-    template<typename TV, typename TE>
-    DisjointSetGrafoNoDirigido(UnDirectedGraph<TV, TE> &grafo){
+    template <typename TV, typename TE>
+    DisjointSetGrafoNoDirigido(UnDirectedGraph<TV, TE> &grafo)
+    {
         sets = 0;
-        for(auto vertice: grafo.vertexes){
+        for (auto vertice : grafo.vertexes)
+        {
             almacen[vertice.first] = {vertice.first, 0};
             sets++;
         }
     }
-    string Find(string id){
-        return (almacen[id]!=id?Find(almacen[id]):id);
+    string Find(string id)
+    {
+        return (almacen[id] != id ? Find(almacen[id]) : id);
     }
-    bool Union(string id1, string id2){
+    bool Union(string id1, string id2)
+    {
         string raiz1 = Find(id1), raiz2 = Find(id2);
-        if(raiz1!=raiz2){
+        if (raiz1 != raiz2)
+        {
             almacen[raiz2] = raiz1;
             sets--;
             return true;
         }
         return false;
     }
-    int getSets(){return sets;}
-    ~DisjointSetGrafoNoDirigido(){}
+    int getSets() { return sets; }
+    ~DisjointSetGrafoNoDirigido() {}
 };
 
 #endif
--- Graph/Algorithms/data_structures/node.h
+++ Graph/Algorithms/data_structures/node.h
@@ -4,37 +4,44 @@
 
 using namespace std;
 
-template<typename TE>
-struct distance_pair{
+template <typename TE>
+struct distance_pair
+{
   string nodo;
   TE peso;
 
-  bool operator==(distance_pair<TE> otro){
-    return nodo == otro.nodo && peso == otro.peso; 
+  bool operator==(distance_pair<TE> otro)
+  {
+    return nodo == otro.nodo && peso == otro.peso;
   }
 
-  bool operator!=(distance_pair<TE> otro){
+  bool operator!=(distance_pair<TE> otro)
+  {
     return nodo != otro.nodo || peso != otro.peso;
   }
-  bool operator<(distance_pair<TE>otro){
+  bool operator<(distance_pair<TE> otro)
+  {
 
     return peso == otro.peso ? nodo < otro.nodo : peso < otro.peso;
   }
-  bool operator>(distance_pair<TE>otro){
+  bool operator>(distance_pair<TE> otro)
+  {
 
     return peso == otro.peso ? nodo > otro.nodo : peso > otro.peso;
   }
 };
 
 template <typename TE>
-struct NodeBT {
+struct NodeBT
+{
   distance_pair<TE> par;
-  NodeBT* left; 
-  NodeBT* right;  
+  NodeBT *left;
+  NodeBT *right;
 
-  NodeBT() :par{}, left{nullptr}, right{nullptr} {}   
-  NodeBT(distance_pair<TE> par) : par{par}, left{nullptr}, right{nullptr} {}   
-  void KillSelf(){
+  NodeBT() : par{}, left{nullptr}, right{nullptr} {}
+  NodeBT(distance_pair<TE> par) : par{par}, left{nullptr}, right{nullptr} {}
+  void KillSelf()
+  {
     delete this;
   }
 };
